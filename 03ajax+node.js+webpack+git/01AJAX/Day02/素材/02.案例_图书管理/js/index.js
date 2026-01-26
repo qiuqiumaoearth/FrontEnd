2c6d0ce5e29585f0021ac367152b3403672bd884
qiuqiumaoearth <2016566910@qq.com>
2026-01-26 22:49:10 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   "03ajax+node.js+webpack+git/01AJAX/Day02/\347\264\240\346\235\220/02.\346\241\210\344\276\213_\345\233\276\344\271\246\347\256\241\347\220\206/index.html"
    	modified:   "03ajax+node.js+webpack+git/01AJAX/Day02/\347\264\240\346\235\220/02.\346\241\210\344\276\213_\345\233\276\344\271\246\347\256\241\347\220\206/js/index.js"

--- 03ajax+node.js+webpack+git/01AJAX/Day02/素材/02.案例_图书管理/js/index.js
+++ 03ajax+node.js+webpack+git/01AJAX/Day02/素材/02.案例_图书管理/js/index.js
@@ -3,3 +3,88 @@
  *  1.1 获取数据
  *  1.2 渲染数据
  */
+
+const creator = '老李'
+//封装- 获取并渲染图书馆列表函数
+function getBooksList() {
+  //1.获取数据
+  axios({
+    url: 'http://hmajax.itheima.net/api/books',
+    params: {
+      creator
+
+    }
+  }).then(result => {
+    // console.log(result);
+    const bookList = result.data.data
+    console.log(bookList);
+    // {id: 793010, bookname: 'ASDhj', author: 'SADASDASD', publisher: 'ASDASDASD'}
+
+    document.querySelector('.list').innerHTML = bookList.reduce((a, item, index) => a + `<tr>
+          <td>${index + 1}</td>
+          <td>${item.bookname}</td>
+          <td>${item.author}</td>
+          <td>${item.publisher}</td>
+          <td data-id = '${item.id}'>
+            <span class="del">删除</span>
+            <span class="edit">编辑</span>
+          </td>
+        </tr>`, '')
+
+  })
+
+}
+
+getBooksList()
+
+//创建弹框对象
+const addModalDom = document.querySelector('.add-modal')
+const addModal = new bootstrap.Modal(addModalDom, {
+  backdrop: 'static',
+  keyboard: false
+})
+
+// 保存按钮 -> 点击 -> 隐藏弹框
+document.querySelector('.add-btn').addEventListener('click', () => {
+  const addForm = document.querySelector('.add-form')
+  const bookObj = serialize(addForm, { hash: true, emty: true })
+  // console.log(bookObj);
+  //{bookname: '1111', author: '1111', publisher: '1111'}
+  //提交到服务器
+  axios({
+    url: 'http://hmajax.itheima.net/api/books',
+    method: 'POST',
+    data: {
+      ...bookObj,
+      creator
+    }
+  }).then(() => {
+    // console.log(result);
+    getBooksList()
+    //重置表单
+    addForm.reset()
+
+    //隐藏表单
+    addModal.hide()
+
+  })
+})
+
+//删除元素 -> 点击事件
+document.querySelector('.list').addEventListener('click', e => {
+  // console.log(e.target);
+  if (e.target.classList.contains('del')) {
+    const theId = e.target.parentNode.dataset.id
+    console.log(theId);
+    //调用删除接口
+    axios({
+      url: `http://hmajax.itheima.net/api/books/${theId}`,
+      method: 'DELETE',
+
+    }).then(() => {
+      getBooksList()
+
+    })
+  }
+})
+
